f18ad2cc01488e3ebc24d113a5ad0df16b9dbe7e
lescg2207 <leslieal2202@gmail.com>
2023-06-17 17:56:59 -0500

    .

---
+++ wcfVeterinaria/serviceUsers.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcfVeterinaria
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "serviceUsers" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione serviceUsers.svc o serviceUsers.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class serviceUsers : IserviceUsers
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
