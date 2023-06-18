745a4b251564ad50bdb40a0ce73fee0b2214482c
lescg2207 <leslieal2202@gmail.com>
2023-06-18 17:15:57 -0500

    svAlmacen

---
+++ wcfVeterinaria/serviceAlmacen.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcfVeterinaria
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "serviceAlmacen" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione serviceAlmacen.svc o serviceAlmacen.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class serviceAlmacen : IserviceAlmacen
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
